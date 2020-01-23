e1b8da2918681bb98edd2c3ad7c5521f62145475
romanitwork <workromanmail@gmail.com>
2020-01-23 17:23:44 +0200

    done task-5

---
+++ js/task-5.js
@@ -0,0 +1,28 @@
+"use strict";
+
+const products = [
+  { name: "Радар", price: 1300, quantity: 4 },
+  { name: "Сканер", price: 2700, quantity: 3 },
+  { name: "Дроид", price: 400, quantity: 7 },
+  { name: "Захват", price: 1200, quantity: 2 }
+];
+
+const getAllPropValues = function(arr, prop) {
+  const target = [];
+  for (const item of arr) {
+    let targetItem = item[prop];
+    if (targetItem != undefined) {
+      target.push(targetItem);
+    }
+  }
+  return target;
+};
+
+/*
+ * Вызовы функции для проверки работоспособности твоей реализации.
+ */
+console.log(getAllPropValues(products, "name")); // ['Радар', 'Сканер', 'Дроид', 'Захват']
+
+console.log(getAllPropValues(products, "quantity")); // [4, 3, 7, 2]
+
+console.log(getAllPropValues(products, "category")); // []
