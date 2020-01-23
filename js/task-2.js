6d4d3407b9bb9e7c9139b038f4f4340fa51acf5d
romanitwork <workromanmail@gmail.com>
2020-01-23 15:38:16 +0200

    done task-2

---
+++ js/task-2.js
@@ -0,0 +1,18 @@
+"use strict";
+
+const countProps = function(obj) {
+  let counter = 0;
+  for (let key in obj) {
+    counter++;
+  }
+  return counter;
+};
+
+/*
+ * Вызовы функции для проверки работоспособности твоей реализации.
+ */
+console.log(countProps({})); // 0
+
+console.log(countProps({ name: "Mango", age: 2 })); // 2
+
+console.log(countProps({ mail: "poly@mail.com", isOnline: true, score: 500 })); // 3
